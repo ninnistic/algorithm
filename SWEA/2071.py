74f15c06de14acd4d6e36b27f60160d7e38deb6f
ninnistic <parkninni@icloud.com>
2023-01-20 09:16:47 +0900

    Change directories/files

---
+++ SWEA/2071.py
@@ -0,0 +1,18 @@
+# 내 풀이.. 틀림
+# T = int(input())
+
+# for i in range(1, T + 1):
+#     res = 0
+#     input_num = list(map(int, input().split()))
+#     for j in input_num:
+#         res += j
+# avg = round(res/len(input_num))
+# print(f'#{i} {avg}')
+
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    a = list(map(int, input().split()))
+    avg = round(sum(a)/len(a))
+print(f'#{test_case} {avg}')
