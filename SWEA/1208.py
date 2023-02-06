5c092489a4d855571e9834538a0f5383b3f27e25
ninnistic <bewithu14@gmail.com>
2023-02-06 21:29:59 +0900

    20230204

--- SWEA/1208.py
+++ SWEA/1208.py
@@ -1,13 +1,11 @@
 def get_max_idx():
     max_value = 0
     max_idx = -1
-
     for i in range(len(boxes)):
         if boxes[i] > max_value:
             max_value = boxes[i]
             max_idx = i  # max를 구하면서, 갱신되는 시점의 idx까지 저장함.
     return max_idx
-    pass
 
 
 def get_min_idx():
@@ -18,16 +16,14 @@ def get_min_idx():
             min_value = boxes[i]
             min_idx = i
     return min_idx
-    pass
 
 
 for i in range(1, 11):
     n = int(input())  # 덤프 힛수
-    result = 0
     boxes = list(map(int, input().split()))
     for i in range(n):
         boxes[get_max_idx()] -= 1
         boxes[get_min_idx()] += 1
 
-    res = boxes[get_max_idx()] - boxes[get_min_idx()]
+    result = boxes[get_max_idx()] - boxes[get_min_idx()]
     print(f'#{i} {result}')
