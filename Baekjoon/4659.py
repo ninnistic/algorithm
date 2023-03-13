762a169fbab264c22c5727e5b6f6c6ec5662cfda
ninnistic <bewithu14@gmail.com>
2023-03-14 00:57:57 +0900

    20230314

---
+++ Baekjoon/4659.py
@@ -0,0 +1,8 @@
+vowels = ['a','e','i','o','u','ee','oo']
+while True:
+    word = input()
+    if word == 'end':
+        break
+    else:
+        word in vowels
+
---
+++ Baekjoon/4999.py
@@ -0,0 +1,7 @@
+j = input()
+d = input()
+
+if d in j:
+    print("go")
+else:
+    print("no")
